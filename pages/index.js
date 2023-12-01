a08c7ce9e5dfc2d75b31ebca5d7f273e537d4214
Stephen Kettley <kettleystephen@gmail.com>
2023-12-01 11:03:32 +0200

    update features list items

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import EventList from "../components/events/EventList";
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featuredEvents}></EventList>
     </div>
   );
 }
