99ba0e62bd7033de4bc32d79e34e095a8033d6f8
Stephen Kettley <kettleystephen@gmail.com>
2023-12-04 17:27:16 +0200

    update data

--- dummy-data.js
+++ dummy-data.js
@@ -1,17 +1,17 @@
 const DUMMY_EVENTS = [
   {
     id: "e1",
-    title: "Programming for everyone",
+    title: "Programming For Everyone",
     description:
       "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    location: "Somestreet 25, 12345 San Somewhereo",
+    location: "Somestreet 25, 12345 San Francisco",
     date: "2021-05-12",
     image: "images/coding-event.jpg",
-    isFeatured: false,
+    isFeatured: true,
   },
   {
     id: "e2",
-    title: "Networking for introverts",
+    title: "Networking For Introverts",
     description:
       "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
     location: "New Wall Street 5, 98765 New Work",
@@ -21,7 +21,7 @@ const DUMMY_EVENTS = [
   },
   {
     id: "e3",
-    title: "Networking for extroverts",
+    title: "Networking For Extroverts",
     description:
       "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
     location: "My Street 12, 10115 Broke City",
